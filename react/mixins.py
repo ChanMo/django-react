63931b3a6f97d1c8de846af13f5b38eec39a2d76
dyou <chen.orange@aliyun.com>
2022-05-12 11:18:13 +0800

    update

--- react/mixins.py
+++ react/mixins.py
@@ -3,10 +3,16 @@ class ReactMixin:
     Export Props for React App
     html include react_props.html
     """
+    app_root = ''
+
+    def get_app_root(self):
+        return self.app_root
+
     def get_props_data(self):
         return {}
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['props'] = self.get_props_data()
+        context['app_root'] = self.get_app_root()
         return context
