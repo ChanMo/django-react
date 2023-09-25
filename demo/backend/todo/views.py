b3a0c1fe463c96917034534a80cf10ce1c20dc72
dyou <chen.orange@aliyun.com>
2023-09-25 13:56:13 +0800

    fixed filepath

--- demo/backend/todo/views.py
+++ demo/backend/todo/views.py
@@ -4,7 +4,7 @@ from react.mixins import ReactMixin
 
 class IndexView(ReactMixin, TemplateView):
     template_name = 'react/react.html'
-    app_root = 'backend/todo/todo.jsx'
+    app_root = 'todo/components/todo.jsx'
     def get_props_data(self):
         return {
             'data': ['Eat Launch', 'Watch a movie', 'Sleep']
